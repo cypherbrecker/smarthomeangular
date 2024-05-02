1b4966ed98a1939336b2b5ac6b17c367ee06fc72
Templom Balázs <76051712+cypherbrecker@users.noreply.github.com>
2024-05-02 17:08:17 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,13 @@
+
 export const environment = {
-  firebase : {
-      apiKey: "PUTURCODEHERE",
-      authDomain: "PUTURCODEHERE",
-      projectId: "PUTURCODEHERE",
-      storageBucket: "PUTURCODEHERE",
-      messagingSenderId: "PUTURCODEHERE",
-      appId: "PUTURCODEHERE",
-      measurementId: "PUTURCODEHERE"
-    }
+    production : false,
+    firebase : {
+        apiKey: "AIzaSyDHdsF8JdwclUp9QdBmxmMYDZQHlgDiax4",
+        authDomain: "smarthomeangular-91886.firebaseapp.com",
+        projectId: "smarthomeangular-91886",
+        storageBucket: "smarthomeangular-91886.appspot.com",
+        messagingSenderId: "966646889329",
+        appId: "1:966646889329:web:54cd765548e132fe2c94d0",
+        measurementId: "G-T80PFXFTND"
+      }
 };
